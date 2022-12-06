9c9794f127c5d46e24c2705c29ec39aba236db90
areyouokayani <aniqahrawat98@hotmail.co.uk>
2022-12-06 17:40:59 +0000

    formatting
    tidy up code :)

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // responsive nav bar
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navBar = document.getElementsByClassName('navBar')[0]
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navBar = document.getElementsByClassName("navBar")[0];
 
-toggleButton.addEventListener('click', () => {
-  navBar.classList.toggle('active')
-})
+toggleButton.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+});
 
 // Slideshow
 
@@ -13,27 +13,30 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
-
